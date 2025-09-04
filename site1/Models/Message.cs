c698710aff9bbb8d6d742c92cc612aebf8d9d6a2
CesarTecun <163237729+CesarTecun@users.noreply.github.com>
2025-09-03 22:23:46 -0600

    Primera migracion y actualizacion

--- site1/Models/Message.cs
+++ site1/Models/Message.cs
@@ -1,14 +1,9 @@
-using System.ComponentModel.DataAnnotations.Schema;
-
 namespace MessageApi.Models;
 
 public class Message
 {
     public int Id { get; set; }
-
-    [Column("Text")] 
     public string MessageText { get; set; } = string.Empty;
-
     public DateTime CreatedAt { get; set; }
     public DateTime? UpdatedAt { get; set; }
 }
